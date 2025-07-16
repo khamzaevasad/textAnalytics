b7b836deeca9171b9295318589dd2480343f0649
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-17 05:15:42 +0900

    Add text area for user input and implement character counting functionality

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const inputArea = document.getElementById("textArea");
+
+inputArea.addEventListener("input", () => {
+  lettersCollection = {};
+
+  const inputValue = inputArea.value;
+
+  totalCharacter();
+  console.log(letterCounter);
+
+  const letterList = inputValue.split("").forEach((letter) => {
+    if (lettersCollection[letter.toLowerCase()]) {
+      lettersCollection[letter.toLowerCase()] += 1;
+    } else {
+      lettersCollection[letter.toLowerCase()] = 1;
+    }
+    console.clear();
+    console.log(lettersCollection);
+
+    for (let i in lettersCollection) {
+      console.log(
+        i,
+        lettersCollection[i],
+        `${Math.round((lettersCollection[i] / inputValue.length) * 100)}%`
+      );
+    }
+  });
+});
