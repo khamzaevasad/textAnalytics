5047c18337941eb9c0a9118e27969a7f30113bfb
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-18 22:07:07 +0900

    Refactor textAnalyze function to improve character counting logic and ensure accurate word count display

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ const textAreaEl = document.getElementById("textArea");
 const totalCharEl = document.getElementById("total-character");
 const wordsCountEl = document.getElementById("words-count");
 
+textAreaEl.addEventListener("input", textAnalyze);
+
 function textAnalyze() {
   // total character
-  const totalChar = textAreaEl.value;
+  const totalChar = textAreaEl.value; // input value
   totalCharEl.textContent = totalChar.length;
 
   // word count
@@ -13,6 +15,26 @@ function textAnalyze() {
     wordCount = 0;
   }
   wordsCountEl.textContent = wordCount;
-}
 
-textAreaEl.addEventListener("input", textAnalyze);
+  const letterCollection = {}; //letter obj
+
+  totalChar.split("").forEach((letter) => {
+    const lowerLetter = letter.toLowerCase();
+
+    if (letterCollection[lowerLetter]) {
+      letterCollection[lowerLetter] += 1;
+    } else {
+      letterCollection[lowerLetter] = 1;
+    }
+  });
+
+  for (let i in letterCollection) {
+    console.log(
+      i,
+      letterCollection[i],
+      `${Math.round((letterCollection[i] / totalChar.length) * 100)}%`
+    );
+  }
+
+  console.log(letterCollection);
+}
